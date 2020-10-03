296106d0297b46257be856ed2b92efa5b8242b79
Salimov Iaroslav <i.salimov@corp.mail.ru>
2020-10-03 16:57:45 +0300

    initialized project, added crud from tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "normalize.css";
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, Store } from "redux";
+import thunk from "redux-thunk";
+import App from "./App";
+import reducer from "./store/reducer";
 
-ReactDOM.render(
-  <React.StrictMode>
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById("root");
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
